2ab574953296f1d5038bc68132dea4715dbb3878
Kuravi H <kuravih@gmail.com>
2025-06-01 23:05:48 -0400

    Add files
    header files in lib folder are links

---
+++ 00_simple/test_ds2484.cpp
@@ -0,0 +1,16 @@
+#include "kato/log.hpp"
+#include "ds2484/ds2484.hpp"
+
+int main()
+{
+    DS2484 bus((DS2484_I2C_BUS_PREFIX + std::to_string(29)).c_str(), false);
+    bus.scan_for_sensors();
+    kato::log::cout << KATO_RED << "tester.cpp::main() : found " << std::setfill('0') << std::setw(2) << bus.samples.size() << " sensors " << KATO_RESET << std::flush;
+    std::cout << '\r'; // or similar logic depending on how kato::log::cout works
+    for (size_t i = 0; i < bus.samples.size(); i++)
+    {
+        bus.led(bus.samples[i].rom, true);
+        bus.led(bus.samples[i].rom, false);
+        kato::log::cout << KATO_RED << "tester.cpp::main() " << std::setfill('0') << std::setw(2) << i + 1 << "/" << std::setfill('0') << std::setw(2) << bus.samples.size() << " : sample " << bus.samples[i] << KATO_RESET << std::endl;
+    }
+}
